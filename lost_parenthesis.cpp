bbf3c500a7ab9080889478f263e8d97577ace7d5
suhyeon lee <81307924+suhyeon0111@users.noreply.github.com>
2021-07-31 23:57:08 +0900

    백준 1541번 잃어버린 괄호 -시도 1차 중 (아직 코드 완성x) -cpp 언어

---
+++ lost_parenthesis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+string s;
+int min;
+
+void find_min(){
+for(int i = 0; i<s.size();i++){
+    
+}
+}
+int main(int argc, char *argv[]){
+    cin>>s;
+    find_min();//문제 해결함수
+    cout<<min;//최소값 출력
+}
