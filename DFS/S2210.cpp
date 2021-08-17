dc4bbff481c3a55cdd9f38a7bcbf374d77108edc
suhyeon lee <81307924+suhyeon0111@users.noreply.github.com>
2021-08-18 02:03:09 +0900

    백준 2210번 숫자판 점프문제
    -DFS알고리즘
    -c++

--- DFS/S2210.cpp
+++ DFS/S2210.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
+#include <set>
 #include <vector>
-#include <algorithm>
 using namespace std;
 //DFS알고리즘
 
-#define MAX 2
+#define MAX 5
 char Matrix[MAX][MAX];
-vector <string> s;
-
+set <string> s; //중복되는 원소 자동 삭제
+vector<char> v; 
 //루프를 돌면서 한번에 좌표를 바꿔주기 위해서
 int x[]={0,0,1,-1};
 int y[]={1,-1,0,0};
 
  
-void DFS(int a, int b,int count, char str[]){
+void DFS(int a, int b){
     //6번을 선택하면 종료함
-    if(count == 6){
-        string str2(str);
-        s.push_back(str);
-        cout << s.size()<<endl;
+    if(v.size()==6){
+        string str = "";
+        for (int i =5;i>=0;i--){
+            str += v[i]; 
+        } 
+        s.insert(str);
+        
         return;
     }
 
     //x,y배열을 순환
     for(int i=0; i<4;i++){
-        int ax = a+x[i];
-        int bx = b+y[i];
-        if(ax>0 && ax<=MAX && bx>0 && bx<=MAX){
+        int A = a+x[i];//x의 인덱스
+        int B = b+y[i];//y의 인덱스
+        if(0<A && A<=MAX && 0<B && B<=MAX){
             //Matrix를 벗어나지 않는다면
-            count += 1;
-            str[count] = Matrix[a][b];
-            DFS(a, b, count, str);
+            v.push_back(Matrix[A][B]);
+            DFS(A, B);
+            v.pop_back();
         }
 
     }
@@ -40,26 +43,23 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
+ 
     //입력 루프
     for(int i=1; i<=MAX ; i++){
         for(int j=1; j<=MAX ; j++){
-            cin>>Matrix[i][j];
+            cin>> Matrix[i][j];
         }
     }
 
     for(int i=1; i<=MAX; i++){
         for(int j=1; j<=MAX; j++){
-            char str[]={Matrix[i][j]};
-            DFS(i,j,0,str);
+            v.push_back(Matrix[i][j]);
+            DFS(i,j);
+            v.pop_back();
         }
     }
 
-    //중복 원소 제거
-     sort(s.begin(), s.end());
-     s.erase(unique(s.begin(), s.end()), s.end());
-    
     //개수 출력하기
-    cout<<"개수 출력하기"<<s.size()<<endl;
+    cout<<s.size()<<endl;
     return 0;
 }
