d7e661c201ad5235428a422131de43813e14aeec
suhyeon lee <81307924+suhyeon0111@users.noreply.github.com>
2021-08-17 18:01:10 +0900

    백준 2210번 숫자판 점프문제
    -string을 이용하려 했지만 메모리 할당 문제로 실행이 되지 않음
    -수정 중->int로 다시 해봐야할것같음
    -c++

--- DFS/S2210.cpp
+++ DFS/S2210.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 //DFS알고리즘
 
-#define MAX 5
-string Matrix[MAX][MAX];
+#define MAX 2
+char Matrix[MAX][MAX];
 vector <string> s;
 
 //루프를 돌면서 한번에 좌표를 바꿔주기 위해서
@@ -13,53 +13,53 @@ int x[]={0,0,1,-1};
 int y[]={1,-1,0,0};
 
  
-void DFS(int a, int b,int count, string str){
-    string start = Matrix[a][b];
-
+void DFS(int a, int b,int count, char str[]){
     //6번을 선택하면 종료함
     if(count == 6){
+        string str2(str);
         s.push_back(str);
+        cout << s.size()<<endl;
         return;
     }
 
     //x,y배열을 순환
     for(int i=0; i<4;i++){
-        a = a+x[i];
-        b = b+y[i];
-        if(a>0 && a<=MAX && b>0 && b<=MAX){
+        int ax = a+x[i];
+        int bx = b+y[i];
+        if(ax>0 && ax<=MAX && bx>0 && bx<=MAX){
             //Matrix를 벗어나지 않는다면
-            count++;
-            str = str + Matrix[a][b];
-            DFS(a,b,count, str);
+            count += 1;
+            str[count] = Matrix[a][b];
+            DFS(a, b, count, str);
         }
-    }
 
-        
-    
+    }
 }
 
 int main(){
-        
-    int A;//입력 변수
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     
     //입력 루프
     for(int i=1; i<=MAX ; i++){
         for(int j=1; j<=MAX ; j++){
-            cin>>A;
-            Matrix[i][j]= A;
+            cin>>Matrix[i][j];
         }
     }
-    string str;
 
     for(int i=1; i<=MAX; i++){
         for(int j=1; j<=MAX; j++){
+            char str[]={Matrix[i][j]};
             DFS(i,j,0,str);
         }
     }
+
     //중복 원소 제거
-    sort(s.begin(), s.end());
-    s.erase(unique(s.begin(), s.end()), s.end());
+     sort(s.begin(), s.end());
+     s.erase(unique(s.begin(), s.end()), s.end());
     
     //개수 출력하기
-    cout<<s.size();
+    cout<<"개수 출력하기"<<s.size()<<endl;
+    return 0;
 }
